2c6730cd0958b9006cab0220361a2efecc0430b7
irojas <irojas91.aareon@gmail.com>
2020-03-20 11:41:30 +0100

    Enable ssl connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require("express");
+const express = require("express"),
+	fs = require("fs"),
+	https = require("https");
+
 const app = express();
-const PORT = 5000;
+const PORT = 5000,
+	HTTPS_PORT = 5050;
+
+const options = {
+  key: fs.readFileSync("/etc/httpd/sslCertificate/irojas91dev.ddns.net.key"),
+  cert: fs.readFileSync("/etc/httpd/sslCertificate/irojas91dev.ddns.net.crt")
+}
 
 app.get("/googleservices", function (req, res) {
   res.send("This is a response from node app instanced in Amazon's ec2 instance");
@@ -9,3 +18,5 @@ app.get("/googleservices", function (req, res) {
 app.listen(PORT, () => {
  console.log("Server started at port " + PORT);
 });
+
+https.createServer(options, app).listen(HTTPS_PORT);
