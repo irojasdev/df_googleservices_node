1f38831ba0cc567e1a4f387fb868da22dd243fe0
irojas <irojas91.aareon@gmail.com>
2020-03-24 09:53:29 +0100

    Fix ssh connection

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const express = require("express"),
 const app = express();
 const config = fs.readFileSync("./app_config.json");
 
+const ssh_options = new Object();
+ssh_options.key = fs.readFileSync(config.ssh.key_file);
+ssh_options.cert = fs.readFileSync(config.ssh.cert_file);
+ssh_options.passphrase = config.ssh.passphrase;
+
 app.post("/googleservices", function (req, res) {
 	var response = {
 		fulfillmentText: "This is a response from node app instanced in Amazon's ec2 instance"
@@ -14,4 +19,4 @@ app.post("/googleservices", function (req, res) {
 	res.send(response);
 });
 
-https.createServer(config.ssh_options, app).listen(config.https_port);
+https.createServer(ssh_options, app).listen(config.https_port);
