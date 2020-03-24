7b32d71c22e3d87f3acd4fc3266a702807469bf5
irojas <irojas91.aareon@gmail.com>
2020-03-24 16:36:34 +0100

    Apply middleware pool to webhook endpoint

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express"),
 	fs = require("fs"),
-	https = require("https");
-	//middlewares = require("./middleware/loader");
+	https = require("https")
+	middlewares = require("./middleware/loader");
 
 const app = express();
+const router = express.Router();
 const config = JSON.parse(fs.readFileSync("./app_config.json", "utf8"));
 
 const ssh_options = new Object();
@@ -11,12 +12,6 @@ ssh_options.key = fs.readFileSync(config.ssh.key_file);
 ssh_options.cert = fs.readFileSync(config.ssh.cert_file);
 ssh_options.passphrase = config.ssh.passphrase;
 
-app.post("/googleservices", function (req, res) {
-	var response = {
-		fulfillmentText: "This is a response from node app instanced in Amazon's ec2 instance"
-	};
-
-	res.send(response);
-});
+router.post("/googleservices", middlewares.pool, function (req, res){res.send({fulfillmentText: "Amazon ec2 webhook response"})});
 
 https.createServer(ssh_options, app).listen(config.https_port);
